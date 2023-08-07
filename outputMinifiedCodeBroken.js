bf5ec2cff35592f879966727fc0ff9cff4d81bdd
Jan Lentmaier <jan.lentmaier@sap.com>
2023-08-07 12:17:37 +0000

    Initial Commit npm install and npm run build produces the buggy outputmin...

---
+++ outputMinifiedCodeBroken.js
@@ -0,0 +1,85 @@
+(self.webpackChunkabc_orchestra=self.webpackChunkabc_orchestra||[]).push([[118],{"./node_modules/path/to/abc.js":function(a,s,d){"use strict";function e(a={}){return`
+        abc: cde;
+        def: ${a.def||0};
+        qwert: ${a.qwert||0};
+        asda: ${a.asda||0};
+        left: ${a.abc||0};
+    `}Object.defineProperty(s,"__esModule",{value:!0}),s.MainCSS=s.sHeight=void 0,s.sHeight="asdasd",s.MainCSS=`
+    .ThisshouldOnlyBeonTop {
+        ${e()}
+    .ThisshouldOnlyBeonTop {
+        
+        asda-asdasda: asdad-asdsa;
+    }
+    .abcBlablaOneQSR {
+        line-height: ${s.sHeight}
+    .ThisshouldOnlyBeonTop {
+        
+        asda-asdasda: asdad-asdsa;
+    }
+    .abcBlablaOneQSR {
+        line-height: ;
+        text-shadow: none;
+    }
+    .abcBlablaOne .asdsad {
+        
+        abc: ab;
+        cdef: de;
+        ghn: fg;
+        sadasn: hi;
+    
+    .ThisshouldOnlyBeonTop {
+        
+        asda-asdasda: asdad-asdsa;
+    }
+    .abcBlablaOneQSR {
+        line-height: ;
+        text-shadow: none;
+    }
+    .abcBlablaOne .asdsad {
+        
+        ${e()}
+    .ThisshouldOnlyBeonTop {
+        
+        asda-asdasda: asdad-asdsa;
+    }
+    .abcBlablaOneQSR {
+        line-height: ;
+        text-shadow: none;
+    }
+    .abcBlablaOne .asdsad {
+        
+        
+        
+        asdasd-adsdas: asdasd-asdas;
+        xzczxc: zxczxcc;
+    }
+    .asdasd {
+        ${e({abc:"weqweqw!important",def:s.sHeight+"!important",qwert:"0 !important"})}
+    .ThisshouldOnlyBeonTop {
+        
+        asda-asdasda: asdad-asdsa;
+    }
+    .abcBlablaOneQSR {
+        line-height: ;
+        text-shadow: none;
+    }
+    .abcBlablaOne .asdsad {
+        
+        
+        
+        asdasd-adsdas: asdasd-asdas;
+        xzczxc: zxczxcc;
+    }
+    .asdasd {
+        
+        sdsd: auto !important;
+        xcxcx: auto !important;
+    }
+    .aasdasdasd .asdsada {
+        we: asdasd !important;
+    }
+    .asdasdasd:not(.adsadsad) .asdasdasd {
+        ds: ${parseFloat(s.sHeight)+parseFloat("asdsads")}rem !important;
+    }
+    `}}]);
